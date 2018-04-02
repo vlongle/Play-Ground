117bbb8aad3befa9a94d2c988536261e5ef55945
Long Le <vietlong.lenguyen@gmail.com>
2018-04-01 23:33:22 -0400

    Graph algorithms

--- shortest_path.py
+++ shortest_path.py
@@ -1,18 +1,21 @@
-
+import queue
 
 
 # This algorithm doesn't guarantee shortest path !!
 # Use stack structure 
 def BFS(s, graph):
     levels = {s:0}
-    stack = [s]
-    while len(stack ) > 0:
-        parent = stack.pop()
+    backtrack = {s: None}
+    q = queue.Queue()
+    q.put(s)
+    while not q.empty():
+        parent = q.get()
         for v in graph[parent]:
             if v not in levels:
                 levels[v] = levels[parent] + 1
-                stack.append(v)
-    return levels
+                backtrack[v] = parent
+                q.put(v)
+    return (levels, backtrack)
 
 # Depth-first search
 def DFS(s, graph, visit = []):
@@ -24,12 +27,23 @@ def DFS(s, graph, visit = []):
             DFS(v, graph, visit)
     return visit
 
+# visit all nodes in the graph
 def visit_all(graph):
     visit = []
     for v in graph:
         visit += DFS(v, graph)
     return set(visit)
 
+# s->v
+def shortest_BFS_path(s,v, graph):
+    levels, backtracks = BFS(s, graph)
+    path = [v]
+    current = v
+    while current !=s:
+        back = backtracks[current]
+        path.append(back)
+        current = back
+    return path
 
 
 
@@ -43,6 +57,8 @@ graph = {'A':[ 'B'],
          'F': ['E']
 }
 visit = ['A']
-print(BFS('A', graph))
+levels, backtracks = BFS('A', graph)
+print(levels, backtracks)
+print("shortest",shortest_BFS_path('A', 'A', graph))
 print(DFS('A', graph))
 print(visit_all(graph))
