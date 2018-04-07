28dd14c070ffb83c2bb54e3830b8af1928dbffda
Long Le <vietlong.lenguyen@gmail.com>
2018-04-06 21:50:27 -0400

    Fix bugs in coinExchange to handle more general cases & undefined solns

--- Intro to Algorithms (Thomas Cormen)/Ch-15 DP/coin-exchange/coinExchange.py
+++ Intro to Algorithms (Thomas Cormen)/Ch-15 DP/coin-exchange/coinExchange.py
@@ -11,27 +11,34 @@ def DPCoin(target_bill, denomination, optimal = {}, soln = {}):
     for cash in denomination:
         optimal[cash] = 1
         soln[cash] = [cash]
-    return DPCoin(target_bill, denomination, optimal, soln)
+    return recurse_coin(target_bill, denomination, optimal, soln)
 
 '''Top-down approach using memoization '''
 def recurse_coin(target_bill, denomination, optimal, soln):
+    # if target_bill < min(denomination):
+    #     return (9999999999, None) # basically impossible to do ! Undefined soln
+
     if target_bill in optimal:
         return (optimal[target_bill], [target_bill])
 
     min_coins = 9999999
     min_cash = 0
     for cash in denomination: # loop through all possible choices for the first bill
-        if target_bill >= cash:
+        if target_bill >= cash and cash >= min(denomination):
             sub = recurse_coin(target_bill - cash, denomination, optimal, soln)[0] + 1
             if min_coins > sub:
                 min_coins = sub
                 min_cash = cash
+
     optimal[target_bill] = min_coins
-    soln[target_bill] = [min_cash] + soln[target_bill-min_cash]
+    if min_coins != 9999999: # find a valid soln
+        soln[target_bill] = [min_cash] + soln[target_bill-min_cash]
+    else:
+        soln[target_bill] = [None]
     return (min_coins, soln)
 
 
-denomination = [1,9,15]
+denomination = [3,9,15]
 target_bill = 16
 
 a = [12,34]
