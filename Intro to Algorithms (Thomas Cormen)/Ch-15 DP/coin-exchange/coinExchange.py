c34062b074eea73b01d2100aa140470bc0dbaf31
Long Le <vietlong.lenguyen@gmail.com>
2018-04-09 12:59:53 -0400

    Update coinExchange.py

--- Intro to Algorithms (Thomas Cormen)/Ch-15 DP/coin-exchange/coinExchange.py
+++ Intro to Algorithms (Thomas Cormen)/Ch-15 DP/coin-exchange/coinExchange.py
@@ -15,16 +15,15 @@ def DPCoin(target_bill, denomination, optimal = {}, soln = {}):
 
 '''Top-down approach using memoization '''
 def recurse_coin(target_bill, denomination, optimal, soln):
-    # if target_bill < min(denomination):
-    #     return (9999999999, None) # basically impossible to do ! Undefined soln
-
+    # If it's a subproblem we already solved, simply return the result
     if target_bill in optimal:
         return (optimal[target_bill], [target_bill])
-
+    
+    # Otherwise, it's a new subproblem to be solved
     min_coins = 9999999
     min_cash = 0
     for cash in denomination: # loop through all possible choices for the first bill
-        if target_bill >= cash and cash >= min(denomination):
+        if target_bill >= cash: # target have to be larger than cash to obtain a valid soln
             sub = recurse_coin(target_bill - cash, denomination, optimal, soln)[0] + 1
             if min_coins > sub:
                 min_coins = sub
@@ -33,16 +32,12 @@ def recurse_coin(target_bill, denomination, optimal, soln):
     optimal[target_bill] = min_coins
     if min_coins != 9999999: # find a valid soln
         soln[target_bill] = [min_cash] + soln[target_bill-min_cash]
-    else:
+    else: # meaning the above "for-loop cash in denomination" fails to find a valid soln ---> No soln is possible
         soln[target_bill] = [None]
     return (min_coins, soln)
 
 
+''' This is an example '''
 denomination = [3,9,15]
 target_bill = 16
-
-a = [12,34]
-b = [69]
-
-
-print(DPCoin(target_bill, denomination)[1][target_bill])
+print(DPCoin(target_bill, denomination)[1]) # This should return None beacause 16 is not possible to be changed by 3,9 or 15
